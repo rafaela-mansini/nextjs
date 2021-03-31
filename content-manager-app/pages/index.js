a73d3f019050d51a09e35e6adacca2e211a3388f
Rafaela Mansini <rafaela_mansini@hotmail.com>
2021-03-31 19:53:33 -0300

    Using state inside a class components with setstate

--- content-manager-app/pages/index.js
+++ content-manager-app/pages/index.js
@@ -1,6 +1,34 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
-export default function Home() {
+const ComponentA = () => {
+  return(
+    <>
+      <h1>ComponentA</h1>
+    </>
+  )
+}
+
+class ComponentB extends React.Component{
+  state = {
+    value: 10
+  }
+
+  changeState(incrementor){
+    this.setState({value: incrementor})
+  }
+
+  render(){
+    return(
+      <>
+        Current value CompB: <h1>{ this.state.value }</h1>
+        <button onClick={() => this.changeState(this.state.value+1)}>+</button>
+        <button onClick={() => this.changeState(this.state.value-1)}>-</button>
+      </>
+    )
+  }
+}
+
+function Home() {
   const [value, setValue] = useState(0)
 
   const change = (incrementor) => {
@@ -12,6 +40,10 @@ export default function Home() {
       Current value: <h1>{ value }</h1>
       <button onClick={() => change(+1)}>+</button>
       <button onClick={() => setValue(value-1)}>-</button>
+      <ComponentA />
+      <ComponentB />
     </>
   )
 }
+
+export default Home
