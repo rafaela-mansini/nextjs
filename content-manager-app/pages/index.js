ce6942b0a2439a740f5dbfae526b1e90a8dd0a19
Rafaela Mansini <rafaela_mansini@hotmail.com>
2021-03-31 19:22:57 -0300

    Usestate in react

--- content-manager-app/pages/index.js
+++ content-manager-app/pages/index.js
@@ -1,7 +1,19 @@
-import Head from 'next/head'
+import { useState } from 'react'
 
 export default function Home() {
+  const [value, setValue] = useState(0)
+
+  const increment = () => {
+    setValue(value + 1)
+  }
+
+  const decrement =  () => setValue(value - 1)
+
   return (
-    <h1>Hello World</h1>
+    <>
+      Current value: <h1>{ value }</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </>
   )
 }
