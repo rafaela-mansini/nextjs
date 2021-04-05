03a10daee7904d83d729dbcaf87b457bfa38a715
Rafaela Mansini <rafaela_mansini@hotmail.com>
2021-04-05 19:28:21 -0300

    Next Link href

--- content-manager-app/components/Navbar.js
+++ content-manager-app/components/Navbar.js
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 const Navbar = () => {
 	return (
 		<nav className="navbar">
@@ -22,12 +24,16 @@ const Navbar = () => {
 								</span>
 							</div>
 						</div>
-						<a className="navbar-item is-active is-size-5 has-text-weight-semibold">
-							Home
+						<Link href="/">
+							<a className="navbar-item is-active is-size-5 has-text-weight-semibold">
+								Home
 							</a>
-						<a className="navbar-item is-size-5 has-text-weight-semibold">
-							Examples
-							</a>
+						</Link>
+						<Link href="/resources/new">
+							<a className="navbar-item is-size-5 has-text-weight-semibold">
+								Add
+								</a>
+						</Link>
 						<a className="navbar-item is-size-5 has-text-weight-semibold">
 							Features
 							</a>
---
+++ content-manager-app/pages/resources/new.js
@@ -0,0 +1,13 @@
+import Layout from 'components/Layout'
+
+const ResourceCreate = () => {
+
+  return(
+    <Layout>
+      <div>I'm a resources create page</div>
+    </Layout>
+  )
+
+}
+
+export default ResourceCreate
